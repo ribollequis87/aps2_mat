89041f80535b03509725aaa571c51e124cd3c469
Esther <estherccr@al.insper.edu.com>
2023-11-10 18:51:37 -0300

    aps mais

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calcula_minimo(x, y):
-    interacoes = 1
+def calcula_minimo(x, y, alpha):
+    iteracoes = 1
     a = 6*x + 3 * y + 3
     b = 3*x + 4*y +1
     vetor = (a**2 + b**2)**0.5
-    x_novo = x - 0.1 * a
-    y_novo = y - 0.1 * b
+    x_novo = x - alpha * a
+    y_novo = y - alpha * b
     x = x_novo
     y = y_novo
 
@@ -12,11 +12,15 @@ def calcula_minimo(x, y):
         a = 6*x + 3 * y + 3
         b = 3*x + 4*y +1
         vetor = (a**2 + b**2)**0.5
-        x_novo = x + 0.1 * (-a)
-        y_novo = y + 0.1 * (-b)
+        x_novo = x + alpha * (-a)
+        y_novo = y + alpha * (-b)
         x = x_novo
         y = y_novo
-        interacoes += 1
-    return (f"O x final vale {x}, o y final vale {y} e o número de interações totais foi {interacoes}.")
+        iteracoes += 1
+    return (f"O x final vale {x}, o y final vale {y} e número de interações totais foi {iteracoes}, dado um alpha = {alpha}.")
 
-print(calcula_minimo(0, 0))
+print(calcula_minimo(0, 0, 0.1))
+print(calcula_minimo(0, 0, 0.15))
+print(calcula_minimo(0, 0, 0.2))
+print(calcula_minimo(0, 0, 0.3))
+print(calcula_minimo(0, 0, 0.5))
